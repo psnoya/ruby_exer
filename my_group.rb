3c6e0be837b5f0affefc22632f78e7068bd35028
psnoya <paulina.s.noya@gmail.com>
2020-10-18 16:08:07 +0200

    Created a ruby file

---
+++ my_group.rb
@@ -0,0 +1,4 @@
+my_group = ["Person_1", "Person_2", "Person_3"]
+Person_1 = {name: "Lily" gender: "female" age: 69 }
+Person_2 = {name: "William", gender: "male", age: 18}
+Person_3 = {name: "Hania", gender: "female", age:5}
